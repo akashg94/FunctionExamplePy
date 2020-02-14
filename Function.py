49ba43384bb0318af408e2b2b0217c513eceac0e
akashghosh <akashghosh.nyc@gmail.com>
2020-02-14 14:40:13 -0500

    Initial commit

---
+++ Function.py
@@ -0,0 +1,17 @@
+#adding two numbers with the function
+def add(a, b):
+    sum = a + b
+    return sum
+
+
+result = add(50, 80)
+print("Result = ", result)
+
+#finding the large numbers
+def larrge(c, d):
+    if c > d:
+        return c
+    else:
+        return d
+    maximum = large
+    print(maximum(100, 40))
